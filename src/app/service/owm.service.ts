2d9fa7495e38e266608019d680485d91d8c8c87c
MMC <michaelconfortini@gmail.com>
2020-07-03 01:30:43 +0200

    suppression log

--- src/app/service/owm.service.ts
+++ src/app/service/owm.service.ts
@@ -9,8 +9,7 @@ export class OWMService {
   appId ="9bf7433f52a2c6519df58c3ad150d67e";
   constructor(private http: HttpClient) { }
 
-  async getMeteo(lat, long){
-    console.log(this.appId);    
+  async getMeteo(lat, long){    
     return await this.http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${this.appId}&units=metric`).pipe(first()).toPromise();
   }
 }
