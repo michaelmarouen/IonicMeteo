9a269a22ee8e4732ecc0e1a5e80b056c290136df
MMC <michaelconfortini@gmail.com>
2020-07-03 01:31:34 +0200

    Mise en place des variables et html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Plugins } from '@capacitor/core';
 import { OWMService } from './service/owm.service';
 
@@ -9,19 +9,40 @@ const { Geolocation } = Plugins;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'IonicMeteo';
   data =  {};
-  temps;
+  icon : string;
+  temperature : number ;
+  location : string;
+  min: number ;
+  max: number ;
+  presure: number ;
+  humidity: number;
+  dateduJour;
+
   constructor(private owm: OWMService){}
+  
+
+  async ngOnInit() {
+    await this.getCurrentPosition();
+  }
 
   async getCurrentPosition() {
     const coordinates = await Geolocation.getCurrentPosition();
-    console.log('Curret', coordinates); 
+    const temps :any = await this.owm.getMeteo(coordinates.coords.latitude,coordinates.coords.longitude);   
+    this.data = temps;
     
-    //this.data = {lat : coordinates.coords.latitude, long: coordinates.coords.longitude}
-    const temps = this.owm.getMeteo(coordinates.coords.latitude,coordinates.coords.longitude)   
-    this.data = temps
+    this.icon = await `http://openweathermap.org/img/wn/${temps.weather[0].icon}@2x.png`
+    const theDate= new Date();
+    this.dateduJour = theDate.getDate()+"/"+theDate.getMonth()+"/"+theDate.getFullYear()
+    this.temperature = temps.main.temp;
+    this.location = temps.name;
+    this.min = temps.main.temp_min;
+    this.max = temps.main.temp_max;
+    this.presure = temps.main.pressure;
+    this.humidity = temps.main.humidity;
+
   }
 
   
